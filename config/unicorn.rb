4e31c304f161b9a118867c676faf339835e72ccc
lifan222 <lifandream@gmail.com>
2016-08-21 22:00:38 +0900

    unicorn.rb

---
+++ config/unicorn.rb
@@ -0,0 +1,6 @@
+pid '/var/www/weixin/tmp/unicorn.pid'
+listen '/var/www/weixin/tmp/unicorn.sock'
+stderr_path '/var/www/weixin/log/unicorn.log'
+stdout_path '/var/www/weixin/log/unicorn.log'
+worker_processes 2
+timeout 30
