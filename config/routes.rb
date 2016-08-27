4373d1dd8486af7a0fd1be0c9aaa11f9a1af23c9
lifan222 <lifandream@gmail.com>
2016-08-27 13:28:41 +0900

    signUp

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,7 @@ Rails.application.routes.draw do
   resources :microposts
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'static_pages#home'
-  #get 'static_pages#home'
-
   get 'about' => 'static_pages#about'
   get 'login' => 'static_pages#login'
   get 'signup' => 'users#new'
