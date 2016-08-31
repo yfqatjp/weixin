d8780cf5925e62ff8d2308a1c05ec4e765cb29f4
lifan222 <lifandream@gmail.com>
2016-08-31 17:27:47 +0900

    time

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,13 @@ class User < ApplicationRecord
   has_many :user_courses
   has_many :courses, :through => :user_courses
 
+  after_initialize :init
+
+  def init
+    self.buytime  ||= 0.0
+    self.playtime  ||= 0.0
+  end
+
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
