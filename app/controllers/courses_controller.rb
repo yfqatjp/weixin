6a03668027fb3476e0427391c113ff491252995c
lifan222 <lifandream@gmail.com>
2016-08-31 23:18:31 +0900

    99%

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,13 +4,14 @@ class CoursesController < ApplicationController
     @current_course = @user.courses.find_by(id: params[:course_now])
     scheduler = Rufus::Scheduler.new
     job = scheduler.schedule_every '1m' do
-      if @user.playtime < @user.buytime*3600 && !@current_course.nil?
+      if  request.original_url.length >= 45 && @user.playtime < @user.buytime*3600
         @user.playtime += 60
         @user.save
       else
         job.kill
       end
-
     end
   end
+
 end
+
