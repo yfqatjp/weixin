68c1d675715cba7be967870250610fcd107cfeac
lifan222 <lifandream@gmail.com>
2016-09-16 22:41:43 +0900

    ifram to video

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                         format: { with: VALID_EMAIL_REGEX },
                         uniqueness: { case_sensitive: false }
-  has_secure_password
+  # has_secure_password
   validates :password_digest, length: { minimum: 6 }
 end
